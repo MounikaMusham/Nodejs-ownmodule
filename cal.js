026dea30d655994f57a4bc2ef030fc98ad602b77
mounika <mounikamusham12@gmail.com>
2022-01-03 05:16:12 +0530

    own module

---
+++ cal.js
@@ -0,0 +1,13 @@
+exports.add = function(x,y,z){
+    return x+y+z;
+};
+
+exports.sub = function(x,y,z){
+    return x-y-z;
+};
+exports.mul = function(x,y,z){
+    return x*y*z;
+};
+exports.div = function(x,y,z){
+    return x/y/z;
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+var calculator = require('./cal.js')
+var x=25, y=15; var z=5;
+console.log("addition of a,b and c is"+" "+calculator.add(x,y,z));
+console.log("substraction of a,b and c is"+" "+calculator.sub(x,y,z));
+console.log("multiplication of a,b and c is"+" "+calculator.mul(x,y,z));
+console.log("division of a,b and c is"+" "+calculator.div(x,y,z));
